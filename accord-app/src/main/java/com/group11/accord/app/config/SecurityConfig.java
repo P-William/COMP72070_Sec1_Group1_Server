a329af4db35d41a6ef10d701f6d6c3f271b23448
P-William <w.paetz@uzwelo.com>
2025-02-24 16:50:52 -0500

    Security config

--- accord-app/src/main/java/com/group11/accord/app/config/SecurityConfig.java
+++ accord-app/src/main/java/com/group11/accord/app/config/SecurityConfig.java
@@ -1,23 +1,68 @@
 package com.group11.accord.app.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+
+import java.util.List;
+
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for simplicity
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/swagger").permitAll()
-                        .anyRequest().authenticated()
-                );
 
+    private static final List<String> ALLOWED_PATHS = List.of(
+        "/",
+        "/ws/"
+    );
+
+    private static final List<String> WS_TOPICS = List.of(
+        "/server",
+        "/channel",
+        "/user",
+        "/message"
+    );
+
+    @Bean
+    public SecurityFilterChain securityFiIterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorizedRequests ->
+                authorizedRequests
+                    .anyRequest().access(authorizedRequestManager())
+            );
         return http.build();
+    }
+
+    private AuthorizationManager<RequestAuthorizationContext> authorizedRequestManager() {
+        return (authentication, context) -> {
+            String requestUrl = context.getRequest().getRequestURL().toString();
+            String requestUri = context.getRequest().getRequestURI();
+            String cfHeader = context.getRequest().getHeader("CF-Connecting-IP");
+            String remoteAddr = context.getRequest().getRemoteAddr();
+
+            log.info("Request URL: {}", requestUrl);
+            log.info("Request URI: {}", requestUri);
+            log.info("CF Header: {}", cfHeader);
+            log.info("Remote Addr: {}", remoteAddr);
+
+            boolean isLocalRequest = cfHeader == null &&
+                (remoteAddr.startsWith("192.168.") || remoteAddr.startsWith("127.0.") || remoteAddr.startsWith("localhost"));
+            boolean isCloudflareRequest = cfHeader != null &&
+                requestUrl.startsWith("http://accord-api.the-hero.dev");
+
+            if (ALLOWED_PATHS.stream().anyMatch(requestUri::startsWith)) {
+                return new AuthorizationDecision(isLocalRequest || isCloudflareRequest);
+            } else {
+                return new AuthorizationDecision(isLocalRequest);
+            }
+        };
     }
 }
