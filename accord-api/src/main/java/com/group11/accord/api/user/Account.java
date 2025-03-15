63a6ddfeb89995511d295f123def1a9af79c6b5f
P-William <w.paetz@uzwelo.com>
2025-03-15 11:10:54 -0400

    same thing

--- accord-api/src/main/java/com/group11/accord/api/user/Account.java
+++ accord-api/src/main/java/com/group11/accord/api/user/Account.java
@@ -16,12 +16,11 @@ public record Account(
         @NonNull
         @Schema(description = "The account's username")
         String username,
-        
+
         @Schema(description = "The account's biography")
         @Size(max = 512, message = "Users biography can be a maximum of 512 characters")
         String bio,
-
-        @NonNull
+        
         @Schema(description = "A link to the account's profile picture")
         String profilePicUrl
 ) { }
