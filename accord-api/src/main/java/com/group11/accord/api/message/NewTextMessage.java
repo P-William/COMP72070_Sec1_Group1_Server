15950bcc1be0f62dec7180263d154128f2ce67df
P-William <w.paetz@uzwelo.com>
2025-04-02 14:32:56 -0400

    fixed missing notnull

--- accord-api/src/main/java/com/group11/accord/api/message/NewTextMessage.java
+++ accord-api/src/main/java/com/group11/accord/api/message/NewTextMessage.java
@@ -1,10 +1,13 @@
 package com.group11.accord.api.message;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
-public record NewTextMessage (
+public record NewTextMessage(
 
     @Schema(description = "The message's content")
+    @NotNull(message = "Content is required")
     String content
 
-) { }
+) {
+}
