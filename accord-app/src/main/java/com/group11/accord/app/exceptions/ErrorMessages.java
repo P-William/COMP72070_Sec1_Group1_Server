1024d1acfed1fdcdf7f42de7802350027859a90e
Connor Miller <miller77fb@gmail.com>
2025-04-02 09:21:06 -0400

    Updated one error message to be more informative

--- accord-app/src/main/java/com/group11/accord/app/exceptions/ErrorMessages.java
+++ accord-app/src/main/java/com/group11/accord/app/exceptions/ErrorMessages.java
@@ -2,7 +2,7 @@ package com.group11.accord.app.exceptions;
 
 public class ErrorMessages {
 
-    public static final String INVALID_CREDENTIALS = "Invalid Login Credentials";
+    public static final String INVALID_CREDENTIALS = "Invalid Login or Session Credentials";
     public static final String MISSING_ACCOUNT_WITH_ID = "Unknown account with ID %s";
     public static final String MISSING_ACCOUNT_WITH_USERNAME = "Unknown account with username %s";
     public static final String FRIEND_REQUEST_ALREADY_EXISTS = "A friend request to %s has already been sent";
